6b93976fc8413bd90cda85d505645994dd24c439
MarcDevGuy127 <marceloh.github@gmail.com>
2026-03-31 21:13:51 -0300

    feat(ItemAcervo): add ifs on setters

--- exemplo_biblioteca/src/entities/ItemAcervo.java
+++ exemplo_biblioteca/src/entities/ItemAcervo.java
@@ -1,5 +1,37 @@
 package entities;
 
 public class ItemAcervo {
+	private String titulo, ano;
+	private int codigo;
+
+	private String getTitulo() {
+		return titulo;
+	}
+
+	private void setTitulo(String titulo) {
+		if (titulo != "") {
+			this.titulo = titulo;
+		}
+	}
+
+	private String getAno() {
+		return ano;
+	}
+
+	private void setAno(String ano) {
+		if (ano != "") {
+			this.ano = ano;
+		}
+	}
+
+	private int getCodigo() {
+		return codigo;
+	}
+
+	private void setCodigo(int codigo) {
+		if (codigo > 0) {
+			this.codigo = codigo;
+		}
+	}
 
 }
