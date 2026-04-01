c9036dd01918189e13396a9f2f4719b28c422f65
MarcDevGuy127 <marceloh.github@gmail.com>
2026-03-31 21:01:58 -0300

    feat(Usuario): add is on setters.

--- exemplo_biblioteca/src/entities/Usuario.java
+++ exemplo_biblioteca/src/entities/Usuario.java
@@ -3,41 +3,64 @@ package entities;
 public class Usuario {
 	private String nome, cidade, bairro, endereco, complemento;
 	private int idade;
-	
+
 	private String getNome() {
 		return nome;
 	}
+
 	private void setNome(String nome) {
-		this.nome = nome;
+		if (nome != "") {
+			this.nome = nome;
+		}
 	}
+
 	private String getCidade() {
 		return cidade;
 	}
+
 	private void setCidade(String cidade) {
-		this.cidade = cidade;
+		if (nome != "") {
+			this.cidade = cidade;
+		}
 	}
+
 	private String getBairro() {
 		return bairro;
 	}
+
 	private void setBairro(String bairro) {
-		this.bairro = bairro;
+		if (nome != "") {
+			this.bairro = bairro;
+		}
 	}
+
 	private String getEndereco() {
 		return endereco;
 	}
+
 	private void setEndereco(String endereco) {
-		this.endereco = endereco;
+		if (endereco != "") {
+			this.endereco = endereco;
+		}
 	}
+
 	private String getComplemento() {
 		return complemento;
 	}
+
 	private void setComplemento(String complemento) {
-		this.complemento = complemento;
+		if (complemento != "") {
+			this.complemento = complemento;
+		}
 	}
+
 	private int getIdade() {
 		return idade;
 	}
+
 	private void setIdade(int idade) {
-		this.idade = idade;
+		if (idade > 0) {
+			this.idade = idade;
+		}
 	}
 }
