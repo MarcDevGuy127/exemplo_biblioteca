069c60140aa92ac9bfadaf4f4d1791b741fca631
MarcDevGuy127 <marceloh.github@gmail.com>
2026-03-31 21:19:52 -0300

    feat(JogoEducativo): adds ifs on setters

--- exemplo_biblioteca/src/entities/JogoEducativo.java
+++ exemplo_biblioteca/src/entities/JogoEducativo.java
@@ -1,5 +1,37 @@
 package entities;
 
 public class JogoEducativo {
+	private String nome, descricao;
+	private int pecas;
+
+	private String getNome() {
+		return nome;
+	}
+
+	private void setNome(String nome) {
+		if (nome != "") {
+			this.nome = nome;
+		}
+	}
+
+	private String getDescricao() {
+		return descricao;
+	}
+
+	private void setDescricao(String descricao) {
+		if (descricao != "") {
+			this.descricao = descricao;
+		}
+	}
+
+	private int getPecas() {
+		return pecas;
+	}
+
+	private void setPecas(int pecas) {
+		if (pecas != 0) {
+			this.pecas = pecas;
+		}
+	}
 
 }
