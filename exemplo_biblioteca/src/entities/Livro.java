9d3b5aa38545f49f7ea42dd08a45c51ce631d974
MarcDevGuy127 <marceloh.github@gmail.com>
2026-04-03 10:46:57 -0300

    test(Livro): add getters and setters to Livro

--- exemplo_biblioteca/src/entities/Livro.java
+++ exemplo_biblioteca/src/entities/Livro.java
@@ -1,5 +1,30 @@
 package entities;
 
 public class Livro {
-
+	private String titulo, autor, editora;
+	private int numPaginas;
+	private String getTitulo() {
+		return titulo;
+	}
+	private void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	private String getAutor() {
+		return autor;
+	}
+	private void setAutor(String autor) {
+		this.autor = autor;
+	}
+	private String getEditora() {
+		return editora;
+	}
+	private void setEditora(String editora) {
+		this.editora = editora;
+	}
+	private int getNumPaginas() {
+		return numPaginas;
+	}
+	private void setNumPaginas(int numPaginas) {
+		this.numPaginas = numPaginas;
+	}
 }
