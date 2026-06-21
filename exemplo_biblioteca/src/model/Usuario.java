a07cc753496d92a28e39dacf8e44e47f5e859c58
MarcDevGuy127 <marceloh.github@gmail.com>
2026-06-21 18:39:03 -0300

    feat(model): add Usuario class

---
+++ exemplo_biblioteca/src/model/Usuario.java
@@ -0,0 +1,48 @@
+package model;
+
+public abstract class Usuario {
+
+    private String matricula;
+    private String nome;
+    private String curso;
+
+    // definindo cada objeto usuario
+    public Usuario(String matricula, String nome, String curso) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.curso = curso;
+    }
+
+    // aqui matricula eh imutavel e preservada, apenas consultada.
+    public String getMatricula() {
+        return matricula;
+    }
+    
+    // nome do usuario pode ser consultado e alterado
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // o curso pode ser consultado, porem nesse caso ele sera imultavel e preservado.
+    public String getCurso() {
+		return curso;
+	}
+
+    // consultando tipo de usuario
+	public abstract String getTipoUsuario();
+
+    @Override
+    public String toString() {
+        return getMatricula()
+                + " - "
+                + getNome()
+                + " - "
+                + getCurso()
+                + " - "
+                + getTipoUsuario();
+    }
+}
