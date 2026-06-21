2be8161e00a6239e4da35df9364aa100d5d013ec
MarcDevGuy127 <marceloh.github@gmail.com>
2026-06-21 20:54:50 -0300

    feat(strategies): add MultaAlunoStrategy interface

---
+++ exemplo_biblioteca/src/strategies/MultaAlunoStrategy.java
@@ -0,0 +1,27 @@
+package strategies;
+
+import model.Emprestimo;
+import java.time.temporal.ChronoUnit;
+
+//definindo contrato para calculo das multas para Alunos
+public class MultaAlunoStrategy implements CalculadoraMultaStrategy {
+
+    private static final double VALOR_DIARIO = 1.50;
+
+    // sobrescrevendo metodo para calcular multa
+    @Override
+    public double calcularMulta(Emprestimo emprestimo) {
+
+    	// verificando intervalo entre os dias de devolucao e previsao de devolucao
+        long diasAtraso = ChronoUnit.DAYS.between(
+                emprestimo.getDataPrevistaDevolucao(),
+                emprestimo.getDataDevolucao()
+        );
+
+        if (diasAtraso <= 0) {
+            return 0.0;
+        }
+
+        return diasAtraso * VALOR_DIARIO;
+    }
+}
