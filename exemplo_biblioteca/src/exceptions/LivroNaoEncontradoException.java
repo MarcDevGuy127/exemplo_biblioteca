926692df306a47c1280df14dbf533aa374db47ff
MarcDevGuy127 <marceloh.github@gmail.com>
2026-06-21 21:31:01 -0300

    feat(exceptions): add LivroNaoEncontradoException

--- exemplo_biblioteca/src/exceptions/LivroNaoEncontradoException.java
+++ exemplo_biblioteca/src/exceptions/LivroNaoEncontradoException.java
@@ -1,5 +1,6 @@
 package exceptions;
 
+// criando excecao para livro nao encontrado
 public class LivroNaoEncontradoException extends Exception {
 
     public LivroNaoEncontradoException(String mensagem) {
